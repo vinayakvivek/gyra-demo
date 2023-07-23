38acf83ccb3159c5c07a7e04935e5d01718acdad
Vinayak <vinayakdeepu@gmail.com>
2023-07-23 15:43:27 +0530

    ChatBox basic layout with messages and input field

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import CollectionList from "./components/CollectionList";
 const App = () => {
   return (
     <div className="h-screen flex flex-col">
-      <nav className="bg bg-accent p-4">
+      <nav className="bg bg-accent p-4 px-10">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">
           GYRA
         </h1>
       </nav>
 
-      <main className="flex items-stretch grow bg-background p-4 overflow-y-auto">
+      <main className="flex items-stretch grow bg-background p-4 px-10 overflow-y-auto">
         <div className="flex flex-row w-full overflow-y-auto space-x-10">
           <CollectionList className="basis-1/3 pb-2" />
           <ChatBox className="basis-2/3" />
--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,14 +1,50 @@
 import { forwardRef } from "react";
 import { cn } from "../lib/utils";
 import { Card } from "./ui/card";
+import { faker } from "@faker-js/faker";
+import { ScrollArea } from "./ui/scroll-area";
+
+export type UserType = "AI" | "HUMAN";
+
+export interface ChatMessage {
+  id: number;
+  type: UserType;
+  message: string;
+}
 
 const ChatBox = forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => {
+  const chatHistory: ChatMessage[] = [...Array(30)].map((_, index: number) => ({
+    id: index,
+    message: faker.lorem.lines(),
+    type: index % 2 == 0 ? "AI" : "HUMAN",
+  }));
   return (
     <div ref={ref} className={cn("", className)} {...props}>
-      <Card className="h-full">ChatBox</Card>
+      <Card className="h-full flex flex-col p-5">
+        <ScrollArea className="grow">
+          <div className="flex flex-col space-y-2 w-full">
+            {chatHistory.map((item) => (
+              <div key={item.id}>
+                {item.type}: {item.message}
+              </div>
+            ))}
+            {/* <div ref={bottomRef} /> */}
+          </div>
+        </ScrollArea>
+        <div className="w-full">
+          <span className="text-sm italic">AI is typing...</span>
+        </div>
+        <form className="flex flex-row space-x-3 w-full">
+          <input
+            type="text"
+            className="h-10 flex-1 rounded-md px-2 border border-gray-400 focus:outline-none focus:border-gray-600"
+          />
+          <button>Send</button>
+        </form>
+      </Card>
     </div>
   );
 });
