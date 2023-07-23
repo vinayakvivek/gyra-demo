4417b70f31fe858b30139e338ad1bfe13e134fda
Vinayak <vinayakdeepu@gmail.com>
2023-07-23 14:05:54 +0530

    Layout understanding

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
-import ChatBox from "./components/ChatBox";
-import CollectionList from "./components/CollectionList";
+import { faker } from "@faker-js/faker";
 
-function App() {
+const getCollections = () => {
+  const collections = [];
+  for (let i = 0; i < 12; ++i) {
+    collections.push({
+      id: i,
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+    });
+  }
+  return collections;
+};
+
+const App = () => {
   return (
-    <div className="flex flex-row items-center justify-center bg-background h-screen">
-      <div className="h-full basis-1/3 p-12">
-        <CollectionList />
-      </div>
-      <div className="h-full basis-2/3 bg-green-200">
-        <div className="p-12 h-full">
-          <ChatBox />
+    <div className="min-h-screen flex flex-col">
+      <nav className="bg-blue-500 text-white p-4">Navbar Content</nav>
+
+      <main className="flex items-stretch grow bg-red-200 p-4 h-1 overflow-y-auto">
+        <div className="flex w-full bg-red-300 overflow-y-auto">
+          <div className="flex flex-col space-y-5 w-full overflow-y-auto">
+            {getCollections().map((item) => (
+              <div key={item.id}>
+                <div className="flex flex-col space-y-3 border-b">
+                  <p>{item.name}</p>
+                  <p>{item.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      </main>
+
+      <footer className="bg-gray-200 p-2 text-center">
+        <span className="text-sm">{`Made with <3 in Bangalore`}</span>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
