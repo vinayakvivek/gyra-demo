fb79d8658a1806e5dd61ab3958bd367ba6befee1
Vinayak <vinayakdeepu@gmail.com>
2023-07-23 16:11:57 +0530

    Add ChatBoxMessage and styling

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -3,6 +3,10 @@ import { cn } from "../lib/utils";
 import { Card } from "./ui/card";
 import { faker } from "@faker-js/faker";
 import { ScrollArea } from "./ui/scroll-area";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import ReactMarkdown from "react-markdown";
+import clsx from "clsx";
 
 export type UserType = "AI" | "HUMAN";
 
@@ -11,6 +15,30 @@ export interface ChatMessage {
   type: UserType;
   message: string;
 }
+
+const ChatBoxMessage = ({ type, message }: ChatMessage) => {
+  const isHuman = type === "HUMAN";
+  const justifyClass = clsx({ "justify-end": isHuman });
+  return (
+    <div className={cn("flex w-full flex-row", justifyClass)}>
+      <div className={cn("flex w-3/4", justifyClass)}>
+        <div
+          className={cn(
+            "w-fit rounded-xl px-3 py-1 bg-accent",
+            clsx({
+              "bg-chat-human": isHuman,
+            })
+          )}
+        >
+          <ReactMarkdown
+            className="prose selection:bg-yellow-300"
+            children={message}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const ChatBox = forwardRef<
   HTMLDivElement,
@@ -23,26 +51,23 @@ const ChatBox = forwardRef<
   }));
   return (
     <div ref={ref} className={cn("", className)} {...props}>
-      <Card className="h-full flex flex-col p-5">
+      <Card className="h-full flex flex-col p-5 space-y-2">
         <ScrollArea className="grow">
           <div className="flex flex-col space-y-2 w-full">
             {chatHistory.map((item) => (
-              <div key={item.id}>
-                {item.type}: {item.message}
-              </div>
+              <ChatBoxMessage key={item.id} {...item} />
             ))}
             {/* <div ref={bottomRef} /> */}
           </div>
         </ScrollArea>
         <div className="w-full">
-          <span className="text-sm italic">AI is typing...</span>
+          <span className="text-sm text-muted-foreground italic">
+            AI is typing...
+          </span>
         </div>
         <form className="flex flex-row space-x-3 w-full">
-          <input
-            type="text"
-            className="h-10 flex-1 rounded-md px-2 border border-gray-400 focus:outline-none focus:border-gray-600"
-          />
-          <button>Send</button>
+          <Input type="text" placeholder="Type in your query" />
+          <Button>Send</Button>
         </form>
       </Card>
     </div>
