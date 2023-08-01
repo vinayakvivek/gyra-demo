17d3065063ff660112dff24a7704b4ee1c5b028e
Vinayak <vinayakdeepu@gmail.com>
2023-08-01 18:28:18 +0530

    Add react router and admin page

---
+++ src/pages/AdminPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const AdminPage = () => {
+  return (
+    <div className="h-screen flex flex-col">
+      <Navbar className="px-16" />
+      <div className="py-8 px-16">Manage</div>
+    </div>
+  );
+};
+
+export default AdminPage;
